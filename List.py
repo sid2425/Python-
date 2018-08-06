73ffa9e639fed0a7d748591708eeab82426219b5
Thirthala Siddarth <40058352+sid2425@users.noreply.github.com>
2018-08-06 18:33:12 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,45 @@
+"""mylist=['1','2']
+print(mylist)->returns ['1','2']
+
+myList=['1','2']
+print(type(myList)) -> returns <class 'list'>
+
+
+mylist=list(("String1",1,2.5,34))
+print(mylist[1]) -> Usage of list() Constructor  returns String1
+
+mylist=[1,2,3]
+num_list=list((4,5,6))
+new=mylist+num_list
+print(new) -> Concatonation of list can be done 
+
+mylist=[1,2,3]
+mylist[0]=143 
+print(mylist)->reassigning of lists can be done in much more simpler way 
+rather than string 
+
+
+new=List((1,2,3,4,4)) returns name 'List' is not defined => Error 
+print(new)
+
+myList=[1,2,30]
+print(myList[0:2:2]) returns [1]
+
+# Functions used in list:
+myList=[1,2,1]
+myList.append(4)
+print(myList) returns [1, 2, 1, 4]
+
+
+MyList=[1234,3456,56788]
+MyList.pop(1)
+print(MyList) => [1234, 56788]
+
+MyList=[192,1233,445465,666755,2343]
+MyList.sort()
+print(MyList) => [192, 1233, 2343, 445465, 666755]
+
+MyList=[1,2,3,4,5]
+MyList.reverse()
+print(MyList) => [5, 4, 3, 2, 1] 
+"""
